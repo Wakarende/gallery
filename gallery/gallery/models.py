aac8209aa271d2326f81415eeeb50c0eb45da608
Joy Kirii <joykirii@gmail.com>
2021-05-15 08:30:50 +0300

    create Category and Location models

--- gallery/gallery/models.py
+++ gallery/gallery/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-  photo = CloudinaryField('photo')
+  # photo = CloudinaryField('photo')
   name = models.CharField(max_length = 30)
   description = models.CharField(max_length = 80)
   category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
@@ -48,3 +48,39 @@ class Image(models.Model):
 
   class Meta:
     ordering = ['-post_date']
+
+class Category(models.Model):
+  name = models.CharField(max_length = 30)
+
+  def __str__(self):
+    return self.name
+
+  def save_category(self):
+    self.save()
+
+  def delete_category(self):
+    self.delete()
+
+  @classmethod
+  def update_category(cls,id,name):
+    cls.objects.filter(id = id).update(name = name)
+
+class Location(models.Model):
+  name = models.CharField(max_length = 30)
+
+  def __str__(self):
+    return self.name
+
+  def save_location(self):
+    self.save()
+
+  def delete_location(self):
+    self.delete()
+
+  @classmethod
+  def update_location(cls,id,name):
+    cls.objects.filter(id = id).update(name = name)
+
+  @classmethod
+  def display_all_locations(cls):
+    return cls.objects.all()
