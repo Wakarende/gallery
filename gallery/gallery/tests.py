ca2c38c7e7986b6c3bce066b03fb13a3816e2502
Joy Kirii <joykirii@gmail.com>
2021-05-16 18:36:01 +0300

    create save category test

--- gallery/gallery/tests.py
+++ gallery/gallery/tests.py
@@ -60,3 +60,70 @@ class LocationTest(TestCase):
     self.nakuru.save_location()
     self.assertEqual(len(Location.display_all_locations()), 2)
 
+class CategoryTest(TestCase):
+  def setUp(self):
+    """
+    Creates new instance before a test
+    """
+    self.fashion = Category(name='fashion')
+    self.games = Category(name='games')
+
+  def tearDown(self):
+    """
+    Clears database after each test
+    """
+    Category.objects.all().delete()
+
+  def test_category_instance(self):
+    """
+    Test whether the new categories an an instance of the category class
+    """
+    self.assertTrue(isinstance(self.fashion, Category))
+    self.assertTrue(isinstance(self.games, Category))
+
+  def test_save_category(self):
+    """
+    Test that determines whehter new categories are saved to the db
+    """
+    self.fashion.save_category()
+    self.games.save_category()
+    categories = Category.objects.all()
+    self.assertTrue(len(categories) == 2)
+
+    
+
+
+
+
+# class ImageTest(TestCase):
+#   def setup(self):
+#     self.new_category= Category(name='interior')
+#     self.new_location= Location(name='nairobi')
+#     self.new_image= Image(name='kitchen',description='description',photo='image.png',category='interior',location='nairobi')
+
+#   def tearDown(self):
+#     """
+#     Clears Database after each test
+#     """
+#     Image.objects.all().delete()
+#     Category.objects.all().delete()
+#     Location.objects.all().delete()
+  
+#   def test_image_instance(self):
+#     """
+#     test that determines whether a new image created is an instance of the Image class
+#     """
+#     self.assertTrue(isinstance(self.new_image, Image))
+
+#   def test_save_image(self):
+#     """
+#     Test whether new image is added to the db
+#     """
+#     self.new_image.save_image()
+#     all_objects = Image.objects.all()
+#     self.assertTrue(len(all_objects)>0)
+  
+
+
+
+
